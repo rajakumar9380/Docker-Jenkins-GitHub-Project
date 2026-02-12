9a3498d97d4ae9e4ec50e4b12d5de4f82296c5e1
rajakumar9380 <rajakumarck2000@gmail.com>
2026-02-12 11:11:21 +0530

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello! This app is running via Jenkins and Docker!'));
-app.listen(3000, () => console.log('Server ready on port 3000'));
